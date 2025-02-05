e37ab0827c1ca3ef7185ca768556e8baa104bb24
Bianca <bianca.furlanetto440@gmail.com>
2025-02-05 16:38:34 -0300

    inclusão do Authorize e AllowAnonymous

---
+++ DevStreamAPI/Controllers/JogoesController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DevStreamAPI.Data;
+using DevStreamAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace DevStreamAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class JogoesController : ControllerBase
+    {
+        private readonly DevSteamAPIContext _context;
+
+        public JogoesController(DevSteamAPIContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Jogoes
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Jogo>>> GetJogos()
+        {
+            return await _context.Jogos.ToListAsync();
+        }
+
+        // GET: api/Jogoes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Jogo>> GetJogo(Guid id)
+        {
+            var jogo = await _context.Jogos.FindAsync(id);
+
+            if (jogo == null)
+            {
+                return NotFound();
+            }
+
+            return jogo;
+        }
+
+        // PUT: api/Jogoes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutJogo(Guid id, Jogo jogo)
+        {
+            if (id != jogo.JogoId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(jogo).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!JogoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Jogoes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Jogo>> PostJogo(Jogo jogo)
+        {
+            _context.Jogos.Add(jogo);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetJogo", new { id = jogo.JogoId }, jogo);
+        }
+
+        // DELETE: api/Jogoes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteJogo(Guid id)
+        {
+            var jogo = await _context.Jogos.FindAsync(id);
+            if (jogo == null)
+            {
+                return NotFound();
+            }
+
+            _context.Jogos.Remove(jogo);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool JogoExists(Guid id)
+        {
+            return _context.Jogos.Any(e => e.JogoId == id);
+        }
+    }
+}
